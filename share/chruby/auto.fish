148df00bc622a67e92f864dd5e5a06afa35e2066
Josh Black <raskchanky@gmail.com>
2013-11-25 11:55:16 -0800

    allow changing into sub dirs while using the auto script

--- share/chruby/auto.fish
+++ share/chruby/auto.fish
@@ -1,39 +1,16 @@
-set -e RUBY_AUTO_VERSION
+set -e RUBY_VERSION_FILE
 
 function chruby_auto --on-event fish_prompt
-  set -l org (pwd)
-  set -l dir (pwd)
-  set -l found ''
-  set -l rversion ''
-  set -l status_code 0
+	set -l dir (pwd)
+	set -l version_file ''
 
-  while not test "$dir" = '/'
-    set version_file "$dir/.ruby-version"
+  set version_file "$dir/.ruby-version"
 
-    if test -f "$version_file"
-      set rversion (cat "$version_file")
+  if test "$version_file" = "$RUBY_VERSION_FILE"; return
+  else if test -f "$version_file"
+    chruby (cat "$version_file"); or return 1
 
-      if test "$rversion" = "$RUBY_AUTO_VERSION"
-        set found 'true'
-        break
-      else
-        set found 'true'
-        set -gx RUBY_AUTO_VERSION "$rversion"
-
-        chruby "$rversion"; or set status_code 1
-        break
-      end
-    end
-
-    cd $dir/..
-    set dir (pwd)
-  end
-  cd $org
-
-  test $status_code = 1; and return 1
-
-  if begin; test -z "$found"; and test -n "$RUBY_AUTO_VERSION"; end
-    chruby_reset
-    set -e RUBY_AUTO_VERSION
+    set -gx RUBY_VERSION_FILE "$version_file"
+    return
   end
 end
